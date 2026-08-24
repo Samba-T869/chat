37de74323bed307a34f5627eb720de8d2ce6f54f
Boniphace Samba <kimwagangalula45@gmail.com>
2026-08-24 15:12:41 -0700

    Muaji

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = Server(server, {
+const io = new Server(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
